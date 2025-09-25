e1e849c9bd96797f3b3a36f031f9b6127d9a157f
ande310g <andersanders@live.dk>
2025-09-25 15:04:54 +0200

    removed pychache and added game loop

--- astroid/main.py
+++ astroid/main.py
@@ -4,6 +4,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while (True):
         for event in pygame.event.get():
@@ -11,10 +13,10 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        print(dt)
+        dt = clock.tick(60)/1000
         
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        
 
 
 if __name__ == "__main__":
